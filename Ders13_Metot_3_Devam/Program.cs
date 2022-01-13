ecbbf3d38928273532520e526baa363d5f4290dc
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-01-13 14:59:10 +0300

    14.Ders Tamamlandı

---
+++ Ders13_Metot_3_Devam/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ders13_Metot_3_Devam
+{
+    class Program
+    {
+
+        static List<long> TCler = new List<long>();
+        static List<string> adSoyadlar = new List<string>();
+        static List<string> adresler = new List<string>();
+        static List<string> telefonlar = new List<string>();
+
+        static void Main(string[] args)
+        {
+
+            //Basit bir Personel Bilgi Sistemi:
+            // TC(long), adSoyad string, adres string, telefon string için birer tane liste
+            // tanımlayınız. PersonelIndexGetir(tc),PersonelEkleGuncelle(başarılı ise true döndür)
+            // PersonelAra: (TC veya adSoyad ile arama yapacak) tüm bilgileri tek string olarak döndür.
+            //PersonelSil(tc): Remove edecek.
+            //
+            //Ödev: PersonelEkleGuncelle yi PersonelGuncelle ve PersonelEkle olarak yazınız. 
+
+            int index = PersonelIndexGetir(67309105720);
+            Console.WriteLine("index=" + index);
+            bool basarili = PersonelEkleGuncelle(tc: 67309105720,
+                                 adSoyad: "Veysel Perü",
+                                 adres: "SahrayıCedit/Kadıköy...",
+                                 telefon: "05327741763");
+            basarili = PersonelEkleGuncelle(tc: 1111,
+                               adSoyad: "Ezgi",
+                               adres: "Bostancı...",
+                               telefon: "05321111111");
+            basarili = PersonelEkleGuncelle(tc: 22222222222,
+                               adSoyad: "Şükrü",
+                               adres: "Sarıgazi...",
+                               telefon: "05322222222");
+            basarili = PersonelEkleGuncelle(tc: 12345678910,
+                               adSoyad: "Uğur Okan",
+                               adres: "Bağcılar...",
+                               telefon: "05323333333");
+            basarili = PersonelEkleGuncelle(tc: 44444444444,
+                               adSoyad: "Melike",
+                               adres: "Beşiktaş...",
+                               telefon: "05324444444");
+            index = PersonelIndexGetir(67309105720);
+            Console.WriteLine("index=" + index);
+
+            basarili = PersonelEkleGuncelle(tc: 67309105720,
+                                 telefon: "0555 5555555");
+            for (int i = 0; i < TCler.Count; i++)
+            {
+                Console.WriteLine(TCler[i]);
+                Console.WriteLine(adSoyadlar[i]);
+                Console.WriteLine(adresler[i]);
+                Console.WriteLine(telefonlar[i]);
+            }
+            Console.WriteLine(PersonelGetir(45685471025));
+            Console.ReadLine();
+        } // Main sonu 
+
+
+        
+       
+
+        
+
+        static string PersonelGetir(long tc=0, string adSoyad="")
+        {
+            int personelIndex = -1;
+            string personel="";
+            if (adSoyad != "") personel = "Ad soyad " + adSoyad + " bulanamadı";
+            if (tc != 0) personel = "TC " + tc + " bulanamadı";
+            if (tc != 0)
+            {
+                personelIndex = PersonelIndexGetir(tc);
+            }
+            else
+            {
+                //if (adSoyad.Contains(adSoyad))
+                    personelIndex = adSoyad.IndexOf(adSoyad);
+            }
+            if (personelIndex != -1)
+            {
+                personel = Convert.ToString(TCler[personelIndex] + " " +
+                    adSoyadlar[personelIndex] + " " +
+                     adresler[personelIndex] + " " +
+                      telefonlar[personelIndex]);
+            }
+            return personel;
+        }
+        static int PersonelIndexGetir(long tc)
+        {
+            int personelIndex = -1;
+            if (TCler.Contains(tc))
+            {
+                personelIndex = TCler.IndexOf(tc);
+            }
+            return personelIndex;
+        }
+
+        private static bool PersonelEkleGuncelle(long tc = 0, string adSoyad = "", string adres = "", string telefon = "")
+        {
+            int personelIndex = -1;
+            if (tc < 10000000000 || tc > 99999999999)
+            {
+                Console.WriteLine("TC formatı yanlış. (" + tc + ")  Entera basınız.");
+                Console.ReadLine();
+                return false;
+            }
+            if (tc == 0)
+            {
+                Console.WriteLine("TC sıfır olamaz. Entera basınız.");
+                Console.ReadLine();
+                return false;
+            }
+            else
+            {
+                personelIndex = PersonelIndexGetir(tc);
+                if (personelIndex == -1)       // Personel yok, o zaman ekleyeceğiz.
+                {
+                    if (adSoyad == "")
+                    {
+                        Console.WriteLine("Ad Soyad boş olamaz. Entera basınız.");
+                        Console.ReadLine();
+                        return false;
+                    }
+                    TCler.Add(tc);
+                    adSoyadlar.Add(adSoyad);
+                    adresler.Add(adres);
+                    telefonlar.Add(telefon);
+                }
+                else   // TC var, o zaman güncelle!
+                {
+                    if (adSoyad != "") adSoyadlar[personelIndex] = adSoyad;
+                    if (adres != "") adresler[personelIndex] = adres;
+                    if (telefon != "") telefonlar[personelIndex] = telefon;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
+
---
+++ Ders14_Metotlar_3/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ders14_Metotlar_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sayı tahmin oyunu:1' dej 1000 ' e kadar bir sayı tutsun.10 tahmin yaptırsın , küçük ise küçük oldu , büyük ise
+            //büyük oldu desin. Tutturursa "tebrikler tutturdunuz" desin.
+            //Tahminleriniz 12 76 233 456 878 .... yazsın.
+            //List<int> sayilar = new List<int>();
+            //Random rastgele = new Random();
+            //int sayi = rastgele.Next(1, 1001);
+            //Console.WriteLine(sayi);
+            //int tahminSayi = 0;
+            //bool tutturdu = false;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("Sayı giriniz : ");
+            //    int.TryParse(Console.ReadLine(), out tahminSayi);
+
+            //    sayilar.Add(tahminSayi);
+            //    if(tahminSayi<sayi)
+            //    {
+            //        Console.WriteLine("Küçük oldu");
+
+            //    }
+            //    if (tahminSayi>sayi)
+            //    {
+            //        Console.WriteLine("Büyük oldu");
+            //    }
+            //    if(tahminSayi==sayi)
+            //    {
+            //        Console.WriteLine("Tebrikler kazandınız.");
+            //        tutturdu = true;
+            //        break;
+            //    }
+            //}
+
+            //if(!tutturdu)
+            //{
+            //    Console.WriteLine("Malesef bilemediniz :");
+            //    Console.WriteLine("TAhmin ettiğiniz sayılar :");
+            //    foreach (var item in sayilar)
+            //    {
+            //        Console.Write(item + " ");
+            //    }
+            //}
+
+            //-----------------------------------------------------------------------------------------------------------------------
+
+            // 1-20 arasında rastgele 10'ar sayı üretip iki listeye atan ve daha sonra 
+            // iki listede ortak olmayanları başka bir listeye atarak ekrana 
+            // yazdıran program:
+            // Örnek
+            // Liste1 = 1 2 3 4 5 8 12 3 4 9
+            // Liste2 = 2 3 11 13 15 8 5 6 7 9
+            // Liste3 = 1 4 12 4 11 13 15 6 7
+            // 
+            //List<int> liste1 = new List<int>();
+            //List<int> liste2 = new List<int>();
+            //List<int> liste3 = new List<int>();
+            //Random rand = new Random();
+            //Console.Write("Liste1=");
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int sayi = rand.Next(1, 21);
+            //    liste1.Add(sayi);
+            //    Console.Write(sayi + " ");
+            //}
+            //Console.WriteLine();
+            //Console.Write("Liste2=");
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int sayi = rand.Next(1, 21);
+            //    liste2.Add(sayi);
+            //    Console.Write(sayi + " ");
+            //}
+            //Console.WriteLine();
+
+            //foreach (var item in liste1)
+            //{
+            //    if (liste2.Contains(item) == false)
+            //        if (liste3.Contains(item) == false)  // Duplike olmasın diye. 
+            //            liste3.Add(item);
+            //}
+            //foreach (var item in liste2)
+            //{
+            //    if (liste1.Contains(item) == false)
+            //        if (liste3.Contains(item) == false) // Duplike olmasın diye. 
+            //            liste3.Add(item);
+            //}
+            //Console.WriteLine("Birleşim Liste3=");
+            //foreach (var item in liste3)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            //-----------------------------------------------------------------------------------------------------------------------
+            //double r = 34.5;
+            //double pi = 3.14;
+            //double cevre = CevreBul(r, pi);
+            //Console.WriteLine(cevre);
+
+            //Console.WriteLine(Topla());
+            //Console.WriteLine(Topla(b:7));
+            //Console.ReadLine();
+
+
+            ////-----------------------------------------------------------------------------------------------------------------------
+            ///Kendisine gönderilen double dizisini float dizine çeviren MakeItFloat isimli metoduz yazınız 
+
+            ////double[] d = new double[10] { 1.2, 2.3, 3.4, 5, 5, 5, 5, 5, 5, 6 };
+            //////float[] f = MakeItFloat(d);
+            ////int[] i = MakeItInt(d);
+
+            ////foreach (var item in i)
+            ////{
+            ////    Console.WriteLine(item + " ");
+            ////}
+
+
+            //Liste içinde liste
+            //List<object> listem = new List<object>();
+            //List<int> sayilarim1 = new List<int>() { 23, 34, 45, 55 };
+            //List<int> sayilarim2 = new List<int>() { 63, 54, 21 };
+
+            //listem.Add(sayilarim1);
+            //listem.Add(sayilarim2);
+
+            //foreach (List<int> liste in listem)
+            //{
+            //    foreach (var item in liste)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            
+
+
+        } //Main sonu
+
+        static int[] MakeItInt(double[] doubleArray)
+        {
+            int[] intArray = new int[doubleArray.Length];
+            for (int i = 0; i < doubleArray.Length; i++)
+            {
+                intArray[i] = (int)doubleArray[i];
+            }
+            return intArray;
+        }
+        static float[] MakeItFloat(double[] doubleArray)
+        {
+            float[] floatArray = new float[doubleArray.Length];
+            for (int i = 0; i < doubleArray.Length; i++)
+            {
+                floatArray[i] = (float)doubleArray[i];
+            }
+            return floatArray;
+        }
+
+        static double CevreBul(double r, double pi = 3.14159275538)
+        {
+            return r * pi;
+        }
+
+        static double Topla(int a = 10, int b = 20)
+        {
+            return a + b;
+        }
+
+    }
+}
+
+
