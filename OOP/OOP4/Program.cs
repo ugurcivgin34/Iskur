39f4cca10c1f62d793eed49fa233b9647ae0c94f
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-02-15 14:57:46 +0300

    oop

--- OOP/OOP4/Program.cs
+++ OOP/OOP4/Program.cs
@@ -5,6 +5,7 @@ namespace OOP4
 
     class Musteri
     {
+        //COnstructor arası veri alışverişleri
         public int MusID { get; set; }
         public string MusAdi { get; set; }
         public string Adres { get; set; }
@@ -21,11 +22,30 @@ namespace OOP4
             
         }
     }
+
+    class Deneme
+    {
+        const double pi = 3.14; //tanımlanan yerde değişebilir
+        readonly double katsayi = 1.5;
+        public Deneme()
+        {
+            //pi = 2.5;
+            katsayi = 1.1;
+        }
+
+        public void Islem()
+        {
+          
+            katsayi = 5.6;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //6-readonly
+            
+            //7-const
         }
     }
 }
