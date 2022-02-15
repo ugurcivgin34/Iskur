0914b5f22ef12e47b3417899add61a8c8f92e48c
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-02-15 14:52:44 +0300

    OOP

---
+++ OOP/OOP4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP4
+{
+
+    class Musteri
+    {
+        public int MusID { get; set; }
+        public string MusAdi { get; set; }
+        public string Adres { get; set; }
+
+        public Musteri(string ad , string adres):this(1,ad,adres)
+        {
+           
+        }
+        public Musteri(int id , string ad , string adres)
+        {
+            MusID = id;
+            MusAdi = ad;
+            Adres = adres;
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
