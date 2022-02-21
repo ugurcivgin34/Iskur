fd0a01bd07e7f6009de8d064d31d49b60976ac2a
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-02-21 15:03:40 +0300

    .

--- OOP_GenelTekrarOrnek/OOP_GenelTekrarOrnek/Program.cs
+++ OOP_GenelTekrarOrnek/OOP_GenelTekrarOrnek/Program.cs
@@ -6,7 +6,12 @@ namespace OOP_GenelTekrarOrnek
     {
         static void Main(string[] args)
         {
-            //Soru:
+            //Soru:Bir market ürün olarak Bardak,Süt,Yoğurt,KağıtHavlu ,Yumurta gibi ürünleri satmaktadır.Bu ürünlerden bazıları
+            //kırılabilen,bazıları bozulabilen veya belirli bir sürede satılması gereken ürünlerdir.
+            //1-Marketteki tüm ürünleri listeleyen...
+            //2-Urun ekleyebildiğimiz..
+            //3-Çeşitli durumlara göre rapor alabildiğimiz(Süresi geçmiş ürünler , kırık ürünler vb gibi...)
+            //Yukarıdaki durumları göz önüne alarak gerekli sistemi OOP kullanarak modelleyiniz ve yazınız.
         }
     }
 }
---
+++ OOP_GenelTekrarOrnek/OOP_GenelTekrarOrnek/Urun.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_GenelTekrarOrnek
+{
+    
+    abstract class Urun:IEnumerable
+    {
+        public int UrunId { get; set; }
+        public string UrunAdi { get; set; }
+
+      
+
+       
+
+        public IEnumerator GetEnumerator()
+        {
+            return UrunId + "" + UrunAdi;
+        }
+    }
+
+ 
+    class Gıda:Urun
+    {
+        public DateTime SonKullanmaTarihi { get; set; }
+
+    }
+    class SutUrunleri:Gıda,IBozabilir,IKisitliTarih
+    {
+
+    }
+    class HayvanUrunleri:Gıda, IBozabilir, IKisitliTarih,IKirilabilir
+    {
+
+    }
+
+    class Eşya:Urun ,IKirilabilir
+    {
+
+    }
+    interface IKirilabilir
+    {
+
+    }
+    interface IBozabilir
+    {
+
+    }
+    interface IKisitliTarih
+    {
+
+    }
+}
